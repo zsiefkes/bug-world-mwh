fda5782ee10dba92ef823e27e5c9ff42948cdbc6
mwh <mwh@gs1617.AD.ECS.VUW.AC.NZ>
2020-08-27 13:43:29 +1200

    Add two-space movement to Butterfly using overriding

--- BugWorldMH/src/Butterfly.java
+++ BugWorldMH/src/Butterfly.java
@@ -4,4 +4,22 @@ public class Butterfly extends Bug {
 	public Butterfly(String name, int x, int y, int en) {
 		super("Butterfly", name, 'B', x, y, en);
 	}
+	
+	public void move(String dir) {
+		super.move(dir);
+		super.move(dir);
+	}
+	
+	// Could rename this to move instead:
+	public void jump(String dir) {
+		if ("N".equals(dir))
+			setY(getY() - 2);
+		if ("S".equals(dir))
+			setY(getY() + 2);
+		if ("W".equals(dir))
+			setX(getX() - 2);
+		if ("E".equals(dir))
+			setX(getX() + 2);
+	}
+
 }
