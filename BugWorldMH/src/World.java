da2f51fc0710513183dc49323871dd1dd5e71602
mwh <mwh@gs1617.AD.ECS.VUW.AC.NZ>
2020-08-27 13:52:40 +1200

    Add bugs inside World constructor

--- BugWorldMH/src/World.java
+++ BugWorldMH/src/World.java
@@ -9,9 +9,22 @@ public class World {
 	public World(int w, int h) {
 		width = w;
 		height = h;
+		for (int i = 0; i < 10; i++) {
+			int x = (int)(Math.random() * width);
+			int y = (int)(Math.random() * height);
+			if (Math.random() < 0.5) {
+				addBug(new Butterfly("bob", x, y, 50));
+			} else {
+				addBug(new Beetle("bob", x, y, 50));
+			}
+		}
 	}
 	
 	public void addBug(Bug b) {
+		if (b.getX() < 0 || b.getX() >= width
+				|| b.getY() < 0 || b.getY() >= height) {
+			return;
+		}
 		bugs.add(b);
 	}
 }
